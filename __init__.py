1c7d9d38288f7ab52ecd7c63c57be7964fe243dc
petertbernhardt <petertbernhardt@gmail.com>
2014-01-08 15:56:18 -0800

    Added user URL input and better output formatting.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 import urllib2
 from bs4 import BeautifulSoup
 
-request = urllib2.Request("http://www.google.com/")
+url = raw_input("Enter a website URL: ")
+request = urllib2.Request(url)
 response = urllib2.urlopen(request)
 soup = BeautifulSoup(response)
 for a in soup.findAll('a'):
-    print a['href']
+    if a.has_attr('href') and a.string and not a.string.isspace():
+        print "URL: " + a['href'] + ", Title: " + a.string
 
