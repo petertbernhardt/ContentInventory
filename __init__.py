fe617cfb0aed99d23d00bbb0fc2fdd3965e8637d
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2014-01-08 19:41:05 -0800

    Comments
    Added comments explaining the for each loop.

--- __init__.py
+++ __init__.py
@@ -5,7 +5,10 @@ url = raw_input("Enter a website URL: ")
 request = urllib2.Request(url)
 response = urllib2.urlopen(request)
 soup = BeautifulSoup(response)
+# For all <a> tags in the page,
 for a in soup.findAll('a'):
+	# if this tag has an href property and its contents are a non-null string,
     if a.has_attr('href') and a.string and not a.string.isspace():
+		# then print out its URL and its Title
         print "URL: " + a['href'] + ", Title: " + a.string
 
